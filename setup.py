5e3b3da4590dc97f86be2c8afb5f4690c1dd53de
Kelvin Tsoi <ktsoi@wearfigs.com>
2019-09-25 23:10:21 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="klog",
+    version="0.0.1",
+    author="Kelvin Tsoi",
+    author_email="kelvin@hellospork.com",
+    description="Python package for script logging and DataDog integration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tsoisauce/klog",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
